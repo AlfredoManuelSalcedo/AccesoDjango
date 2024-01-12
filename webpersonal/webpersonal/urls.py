3f03e74cbc10ab652452c1e40ea19dff250a2a51
AlfredoManuelSalcedo <150013355+AlfredoManuelSalcedo@users.noreply.github.com>
2024-01-12 12:20:53 +0100

    añadido archivos para su futuro uso

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -23,9 +23,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     ]
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
-    path('admin/', admin.site.urls),
-    path('contact/',views.contact,name="contact"),
-    path('portfolio/',views.portfolio,name="portfolio"),
-    ]
+ path('', views.home, name="home"),
+ path('about-me/', views.about, name="about"),
+ path('portfolio/', views.portfolio, name="portfolio"),
+ path('contact/', views.contact, name="contact"),
+ path('admin/', admin.site.urls),
+]
+
